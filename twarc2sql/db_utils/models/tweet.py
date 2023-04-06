6319252822766f956faf57c96d57d215dc8b2a6b
unna97 <31486108+unna97@users.noreply.github.com>
2023-04-06 23:53:49 +0530

    💩 Add minimal example for the Tweet & Author Model

--- twarc2sql/db_utils/models/tweet.py
+++ twarc2sql/db_utils/models/tweet.py
@@ -1,45 +1,63 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, Boolean, DateTime
-from sqlalchemy.orm import relationship
+"""
+The SQLAlchemy models for creating the database tables.
+The models are based on the Twitter API v2 Tweet object. 
+It is an opnionated normalization of the Tweet object 
+to make it easier to query the database.
+"""
+
+
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
 
 class Tweet(Base):
     __tablename__ = "tweet"
 
-    id = Column(String(256), primary_key=True)
-    created_at = Column(DateTime)
-    text = Column(Text)
-    possibly_sensitive = Column(Boolean)
-    conversation_id = Column(Text)
-    author_id = Column(String(256), ForeignKey("author.id"))
-    source = Column(Text)
-    reply_settings = Column(Text, nullable=True)
-    lang = Column(Text, nullable=True)
-    retweet_count = Column(Integer)
-    like_count = Column(Integer)
-    quote_count = Column(Integer)
-    reply_count = Column(Integer)
-    
+    id = Column(
+        String(256),
+        primary_key=True,
+        nullable=False,
+        unique=True,
+        doc="The unique identifier of the tweet",
+    )
+    created_at = Column(DateTime, doc="The date and time when the tweet was created")
+    text = Column(Text, doc="The text of the tweet")
+    possibly_sensitive = Column(Boolean, doc="Whether the tweet is possibly sensitive")
+    conversation_id = Column(Text, doc="The conversation id of the tweet")
+    author_id = Column(
+        String(256),
+        ForeignKey("author.id"),
+        nullable=False,
+        doc="The author of the tweet",
+    )
+    # TODO: it's a Literal, find the complete list of possible values
+    reply_settings = Column(Text, nullable=True, doc="The reply settings of the tweet")
+    lang = Column(Text, nullable=True, doc="The language of the tweet")
+    # TODO: Add FOREIGN KEY constraint
+    in_reply_to_user_id = Column(
+        String(256),
+        nullable=True,
+        doc="The user id of the user the tweet is replying to",
+    )
     tweet_type = Column(Integer)
 
     author = relationship("Author", back_populates="tweets")
 
 
 class Author(Base):
-
     __tablename__ = "author"
 
-    id = Column(String(256), primary_key=True)
-    name = Column(Text)
-    username = Column(Text)
-    created_at = Column(DateTime)
-    description = Column(Text, nullable=True)
-    
-
-class HashTag(Base):
-    __tablename__ = "hashtag"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    tag = Column(Text)
-    tweet_id = Column(String(256), ForeignKey("tweet.id"))
+    id = Column(
+        String(256),
+        primary_key=True,
+        nullable=False,
+        unique=True,
+        doc="The unique identifier of the author",
+    )
+    name = Column(Text, doc="The name of the author")
+    username = Column(Text, doc="The username of the author")
+    created_at = Column(DateTime, doc="The date and time when the author was created")
+    description = Column(Text, nullable=True, doc="The description of the author")
