aa18cdeb3b7b8c0add73d97a69dbaca82cfab27d
unna97 <31486108+unna97@users.noreply.github.com>
2023-04-23 18:41:48 +0530

    🔖 Release the package : v0.2.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/unna97/twarc2sql",
-    version="0.2.0",
+    version="0.2.1",
     zip_safe=False,
     extras_require=extras_require,
 )
