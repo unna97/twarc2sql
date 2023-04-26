9b42b7c1b51d0fe15580af12dfad47f7fc20aef0
unna97 <31486108+unna97@users.noreply.github.com>
2023-04-27 00:07:41 +0530

    🎨  Add objects-column + referenced tables-columns

--- twarc2sql/file_utils/__init__.py
+++ twarc2sql/file_utils/__init__.py
@@ -29,9 +29,73 @@ object_columns = {
         "in_reply_to_user_id",
         "geo",
     ],
-    "user_object": [],
+    "user_object": [
+        "created_at",
+        "description",
+        "id",
+        "location",
+        "name",
+        "profile_image_url",
+        "protected",
+        "url",
+        "username",
+        "verified",
+        "entities",
+        "pinned_tweet_id",
+        "public_metrics",
+    ],
+    "error_info_object": [
+        "value",
+        "detail",
+        "title",
+        "resource_type",
+        "parameter",
+        "resource_id",
+        "type",
+    ],
+    "media_object": [
+        "media_key",
+        "type",
+        "url",
+        "duration_ms",
+        "height",
+        "preview_image_url",
+        "public_metrics",
+        "width",
+        "alt_text",
+        "variants",
+    ],
+    "places_object": [
+        "full_name",
+        "id",
+        "contained_within",  # optional
+        "name",
+        "geo",
+        "country_code",
+        "country",
+        "place_type",
+    ],
+    "polls_object": [
+        "id",
+        "options",
+        "duration_minutes",
+        "end_datetime",
+        "voting_status",
+    ],
+    "list_object": [
+        "id",
+        "name",
+        "created_at",
+        "description",
+        "follower_count",
+        "member_count",
+        "private",
+        "owner_id",
+    ],
 }
 
+# TODO: Use this to assign names for the tables
+# TODO: Use this to prioritize the order of the tables when creating the database
 table_columns = {
     "tweet": [
         # copy the tweet object columns as is
@@ -68,10 +132,21 @@ table_columns = {
         "url",
         "username",
         "verified",
+        # "pinned_tweet_id",
         # Expanded columns
         "followers_count",
         "following_count",
         "tweet_count",
         "listed_count",
+    ],
+    "retweeted_tweet_mapping": [
+        "tweet_id",
+        "id",
+    ],
+    "quoted_tweet_mapping": ["tweet_id", "id"],
+    "replied_to_tweet_mapping": [
+        "id",
+        "tweet_id",
+        "in_reply_to_user_id",
     ],
 }
